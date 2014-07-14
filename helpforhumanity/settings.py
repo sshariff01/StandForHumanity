06ef24dcbc05e765b6d797910eb5505f1c3b49e7
sshariff01 <shoabe.shariff@gmail.com>
2014-07-14 02:50:40 -0400

    Create tagline. Add temp logo. Add temp tagline. Add map with random styles, to be modified. Migrate css from html page to separate css file.

--- helpforhumanity/settings.py
+++ helpforhumanity/settings.py
@@ -82,7 +82,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-# STATIC_URL = '/static/'
+STATIC_URL = '/static/'
 
 # Parse database configuration from $DATABASE_URL
 # import dj_database_url
--- helpforhumanity/urls.py
+++ helpforhumanity/urls.py
@@ -16,3 +16,6 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
