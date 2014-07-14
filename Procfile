d00456388e446ddbe57ca1c2fb83d0f695152028
sshariff01 <shoabe.shariff@gmail.com>
2014-07-13 20:02:30 -0400

    Initialize django app

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn helpforhumanity.wsgi --log-file -
---
+++ helpforhumanity/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/shoabe/Documents/Workspace/HelpForHumanity/helpforhumanity/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
