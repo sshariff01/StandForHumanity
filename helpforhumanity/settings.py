a4389c3dac8f0e8ecaecbc200bf328546813b8cb
sshariff01 <shoabe.shariff@gmail.com>
2014-07-13 20:18:03 -0400

    Configure with mysql database on cleardb addon

--- helpforhumanity/settings.py
+++ helpforhumanity/settings.py
@@ -57,8 +57,10 @@ WSGI_APPLICATION = 'helpforhumanity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'helpforhumanity_dev', # os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': 'root',
+        'PASSWORD': ''
     }
 }
 
