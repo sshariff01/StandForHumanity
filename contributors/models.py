fe0340d998c08abb44184ef843e17929e6636f97
sshariff01 <shoabe.shariff@gmail.com>
2014-07-15 01:23:07 -0400

    Add lat_lng to Contributor model.

--- contributors/models.py
+++ contributors/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Contributor(models.Model):
     facebook_id = models.TextField(default=0)
     contribution_date = models.DateTimeField('date contributed')
+    lat_lng = models.TextField(default=0)
 
     def __unicode__(self):
         return self.contribution_date
